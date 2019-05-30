6478c83e19e6ebe4181c78779f802b78def64055
Kevin Grant <ConsciousK@yahoo.com>
2019-05-29 20:30:38 -0400

    Completed MVP of Card Components using React and JSX.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return (
+      <div className="CardContent">
+          <h3 className="headline">Get Started with React</h3>
+          <p className="content">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+          <div className="url">reactjs.org</div>
+      </div>
+    );
+  };
+
+  export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import './HeaderContent';
+import HeaderContent from './HeaderContent';
+
+function HeaderTitle() {
+    return (
+    <div className="HeaderTitle">  
+        <h3 className="main">Lambda School</h3><span className="stamp">@Lambda School - 26 Jan.</span>
+        <HeaderContent />
+    </div>
+    );
+};
+
+export default HeaderTitle;
