58586e59a4b36cbdac11e8699ecb2f667b287ae3
Kevin Grant <ConsciousK@yahoo.com>
2019-06-01 14:47:22 -0400

    Reverted back to functional component app. Incorrect libraries installed.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,36 +4,35 @@ import CalculatorDisplay from '../src/components/DisplayComponents/CalculatorDis
 import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 
-class App extends React.component {
-  render () {
-    return (
-      <main className="CalculatorContainer">
-        <CalculatorDisplay text="0" />
+const App = () => {
+  return (
+    <main className="CalculatorContainer">
+      <CalculatorDisplay text="0" />
 
-        <div className="NumbersContainer">
-          <ActionButton ButtonStyle="ActionButton" text="clear" />
-          <NumberButton ButtonStyle="NumberButton" text="1" />
-          <NumberButton ButtonStyle="NumberButton" text="2" />
-          <NumberButton ButtonStyle="NumberButton" text="3" />
-          <NumberButton ButtonStyle="NumberButton" text="4" />
-          <NumberButton ButtonStyle="NumberButton" text="5" />
-          <NumberButton ButtonStyle="NumberButton" text="6" />
-          <NumberButton ButtonStyle="NumberButton" text="7" />
-          <NumberButton ButtonStyle="NumberButton" text="8" />
-          <NumberButton ButtonStyle="NumberButton" text="9" />
-          <ActionButton ButtonStyle="ActionButton" text="0" />
-        </div>
+      <div className="NumbersContainer">
+        <ActionButton ButtonStyle="ActionButton" text="clear" />
+        <NumberButton ButtonStyle="NumberButton" text="1" />
+        <NumberButton ButtonStyle="NumberButton" text="2" />
+        <NumberButton ButtonStyle="NumberButton" text="3" />
+        <NumberButton ButtonStyle="NumberButton" text="4" />
+        <NumberButton ButtonStyle="NumberButton" text="5" />
+        <NumberButton ButtonStyle="NumberButton" text="6" />
+        <NumberButton ButtonStyle="NumberButton" text="7" />
+        <NumberButton ButtonStyle="NumberButton" text="8" />
+        <NumberButton ButtonStyle="NumberButton" text="9" />
+        <ActionButton ButtonStyle="ActionButton" text="0" />
+      </div>
 
-        <div className="OperatorsContainer">
-          <NumberButton ButtonStyle="OperatorButton" text="÷"/>
-          <NumberButton ButtonStyle="OperatorButton" text="x"/>
-          <NumberButton ButtonStyle="OperatorButton" text="-"/>
-          <NumberButton ButtonStyle="OperatorButton" text="+"/>
-          <NumberButton ButtonStyle="OperatorButton" text="="/>
-        </div>
+      <div className="OperatorsContainer">
+        <NumberButton ButtonStyle="OperatorButton" text="÷"/>
+        <NumberButton ButtonStyle="OperatorButton" text="x"/>
+        <NumberButton ButtonStyle="OperatorButton" text="-"/>
+        <NumberButton ButtonStyle="OperatorButton" text="+"/>
+        <NumberButton ButtonStyle="OperatorButton" text="="/>
+      </div>
 
-      </main>
-    );
-  }
-}
+    </main>
+  );
+};
+
 export default App;
