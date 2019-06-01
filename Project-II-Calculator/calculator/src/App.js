f405cd8ee5eeab6468029a7fac64d27e4dff2b14
Kevin Grant <ConsciousK@yahoo.com>
2019-06-01 14:22:30 -0400

    Completed MVP of Caclulator APP and reusable Components and Props.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from '../src/components/DisplayComponents/CalculatorDisplay';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="CalculatorContainer">
+      <CalculatorDisplay text="0" />
+
+      <div className="NumbersContainer">
+        <ActionButton ButtonStyle="ActionButton" text="clear" />
+        <NumberButton ButtonStyle="NumberButton" text="1" />
+        <NumberButton ButtonStyle="NumberButton" text="2" />
+        <NumberButton ButtonStyle="NumberButton" text="3" />
+        <NumberButton ButtonStyle="NumberButton" text="4" />
+        <NumberButton ButtonStyle="NumberButton" text="5" />
+        <NumberButton ButtonStyle="NumberButton" text="6" />
+        <NumberButton ButtonStyle="NumberButton" text="7" />
+        <NumberButton ButtonStyle="NumberButton" text="8" />
+        <NumberButton ButtonStyle="NumberButton" text="9" />
+        <ActionButton ButtonStyle="ActionButton" text="0" />
+      </div>
+
+      <div className="OperatorsContainer">
+        <NumberButton ButtonStyle="OperatorButton" text="÷"/>
+        <NumberButton ButtonStyle="OperatorButton" text="x"/>
+        <NumberButton ButtonStyle="OperatorButton" text="-"/>
+        <NumberButton ButtonStyle="OperatorButton" text="+"/>
+        <NumberButton ButtonStyle="OperatorButton" text="="/>
+      </div>
+
     </div>
   );
 };
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ZeroButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button.css';
+
+const ZeroButton = () => {
+    return (
+      <div className="ActionButton">
+        0
+      </div>
+    );
+};
+
+export default ZeroButton;
